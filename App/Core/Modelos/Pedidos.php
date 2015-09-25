31c89024bfe8ae3b0260c507cffa728a707a99ed
ivanmiranda <ivan@pengostores.com>
2015-09-25 10:52:09 -0500

    sesion

--- App/Core/Modelos/Pedidos.php
+++ App/Core/Modelos/Pedidos.php
@@ -5,14 +5,14 @@ class Modelos_Pedidos extends Sfphp_Modelo
 	{
 		$query = "
 			SELECT 
-				FAC.CVE_DOC PEDIDO, FAC.CVE_CLPV, CTE.NOMBRE,PF.
+				FAC.CVE_DOC PEDIDO, FAC.CVE_CLPV, CTE.NOMBRE,
 				FAC.STATUS, FAC.FECHA_DOC, FAC.FECHA_ENT, FAC.FECHA_VEN,
 				FAC.TIP_DOC_SIG
 			FROM FACTP01 FAC
 			INNER JOIN CLIE01 CTE ON CTE.CLAVE = FAC.CVE_CLPV
 			WHERE FAC.STATUS = 'E' AND 
 				FAC.TIP_DOC_SIG IS NULL AND 
-				FAC.FECHA_DOC >= '09-09-2015';
+				FAC.FECHA_DOC >= '09-01-2015';
 		";
 		$where = NULL;
 		return $this->db->query($query);
