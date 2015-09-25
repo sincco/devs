6a039cd4c6c6d01128f546a41ddddb0033b8f4c2
ivanmiranda <ivan@pengostores.com>
2015-09-25 11:42:21 -0500

    Vista pedidos

--- App/Core/Controladores/Pedidos.php
+++ App/Core/Controladores/Pedidos.php
@@ -4,7 +4,6 @@ class Controladores_Pedidos extends Sfphp_Controlador
 	public function inicio()
 	{
 		$this->_vista->pedidos = $this->utf8_converter($this->modeloPedidos->get());
-		echo "ERROR".json_last_error();
 		$this->vistaPedidos;
 	}
 
