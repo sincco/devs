fccd2e72ab020827b5b99665895d1b6b3e0d1303
ivanmiranda <ivan@pengostores.com>
2015-09-25 11:41:17 -0500

    Vista pedidos

--- App/Core/Controladores/Pedidos.php
+++ App/Core/Controladores/Pedidos.php
@@ -3,8 +3,20 @@ class Controladores_Pedidos extends Sfphp_Controlador
 {
 	public function inicio()
 	{
-		$this->_vista->pedidos = array_map('utf8_encode',$this->modeloPedidos->get());
-		#echo "ERROR".json_last_error();
+		#$this->_vista->pedidos = 
+		echo json_encode($this->utf8_converter($this->modeloPedidos->get()));
+		echo "ERROR".json_last_error();
 		$this->vistaPedidos;
+	}
+
+	private function utf8_converter($array)
+	{
+	    array_walk_recursive($array, function(&$item, $key){
+	        if(!mb_detect_encoding($item, 'utf-8', true)){
+	                $item = utf8_encode($item);
+	        }
+	    });
+	 
+	    return $array;
 	}
 }
